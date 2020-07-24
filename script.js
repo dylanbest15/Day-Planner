7bdb8d8f63b5e8bc24e6279f3c3327b292f17f1a
Dylan Best <best.dylan15@gmail.com>
2020-07-24 11:22:13 -0400

    added color coding to timeblocks

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
-// current hour moment
-var currentHour = moment().minutes(0).seconds(0);
-console.log(currentHour.get('hour'));
-
-// 4 hours ago moment
-var fourHoursAgo = currentHour.subtract(4, 'hours');
-console.log(fourHoursAgo.get('hour'));
-
+// variables
 var container = $("div.container");
 var timeblock = $($("#timeblock").html());
+var currentHour = moment().hour();
 
-
-
+// create time blocks
 for (var i = 0; i < 9; i++) {
+
     var hour = moment(i + 9, "HH");
     var newTimeblock = timeblock.clone();
+
+    // hour block
     newTimeblock.find("div.hour p").text(hour.format("hA"));
+
+    // text area
     newTimeblock.find("textarea").attr("id", `textarea-${i+9}`);
+
+    // color coding for past present future
+    if (i + 9 < currentHour) {
+        newTimeblock.find("textarea").addClass("past");
+    }
+    else if (i + 9 > currentHour) {
+        newTimeblock.find("textarea").addClass("future");
+    }
+    else {
+        newTimeblock.find("textarea").addClass("present");
+    }
+
+    // save button
     newTimeblock.find("button.saveBtn").attr("data-target", `textarea-${i+9}`);
+
+    // append to container
     container.append(newTimeblock);
 }
 
